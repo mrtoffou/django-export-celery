74f1945071779bec422136324962d8d4b86cc673
John <johnlam2478@gmail.com>
2021-12-17 22:02:10 -0500

    Update __init__.py

--- src/django_export_celery/__init__.py
+++ src/django_export_celery/__init__.py
@@ -3,5 +3,5 @@ from django.conf import settings
 settings.DJANGO_EXPORT_CELERY_UPLOAD_TO = getattr(
     settings,
     'DJANGO_EXPORT_CELERY_UPLOAD_TO',
-    'django-export-celery-jobs',
+    'django-export-celery-jobs/',
 )
